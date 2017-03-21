5df3dd7aa18189946e1a704a5adec9a0b968c245
Adam Kercheval <arkercheval@gmail.com>
2017-03-20 21:10:44 -0600

    customized car image, changed animation functionality to make movement constant

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ y = 0;
 speed = 5;
 angle = 0;
 mod = 0;
+/* TODO: get LEN and HIG to automatically fill the user's screen width and
+         height, instead of being hard-coded in */
+LEN = 700;
+HIG = 700;
+HBOUND = HIG + 50;
+LBOUND = LEN + 50;
 
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 car = new Image();
-car.src = "http://i.imgur.com/uwApbV7.png";
+car.src = "ye1.png";
 
 window.addEventListener("keydown", keypress_handler, false);
-window.addEventListener("keyup", keyup_handler, false);
 
 var moveInterval = setInterval(function () {
     draw();
@@ -18,7 +23,7 @@ var moveInterval = setInterval(function () {
 
 function draw() {
     context = canvas.getContext("2d");
-    context.clearRect(0, 0, 800, 800);
+    context.clearRect(0, 0, LEN, HIG);
 
     context.fillStyle = "rgb(200, 100, 220)";
     context.fillRect(50, 50, 100, 100);
@@ -28,29 +33,41 @@ function draw() {
 
     context.save();
     context.translate(x, y);
+
+/* this allows Ye to wrap around when he goes out of bounds, but has some
+   worrying corner cases (try driving him off a corner) */
+    if (x > LBOUND) {
+        x = 0;
+        draw();
+    }
+    if (y > HBOUND) {
+        y = 0;
+        draw();
+    }
+    if (x < -50) {
+        x = LBOUND;
+        draw();
+    }
+    if (y < -50) {
+        y = HBOUND;
+        draw();
+    }
     context.rotate(Math.PI / 180 * angle);
     context.drawImage(car, -(car.width / 2), -(car.height / 2));
     context.restore();
 }
 
-function keyup_handler(event) {
-    if (event.keyCode == 87 || event.keyCode == 83) {
-        mod = 0;
-    }
-}
-
 function keypress_handler(event) {
-    console.log(event.keyCode);
-    if (event.keyCode == 87) {
-        mod = 1;
+    if (event.keyCode == 38) { // up arrow
+        mod += 1; // speeds up every time you press up
     }
-    if (event.keyCode == 83) {
-        mod = -1;
+    if (event.keyCode == 40) { // down arrow
+        mod -= 1; // slows down every time you press down
     }
-    if (event.keyCode == 65) {
+    if (event.keyCode == 37) { // left arrow
         angle -= 5;
     }
-    if (event.keyCode == 68) {
+    if (event.keyCode == 39) { // right arrow
         angle += 5;
     }
 }
